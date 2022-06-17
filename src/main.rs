bd31e8993a4cbbea826d597296aeb7d7c9603599
Sygeman <sygeman@gmail.com>
2022-06-17 18:03:39 +0200

    1480. Running Sum of 1d Array

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+struct Solution;
+
+impl Solution {
+    pub fn running_sum(nums: Vec<i32>) -> Vec<i32> {
+        let mut res: Vec<i32> = vec![nums[0]];
+        
+        for (i, v) in nums.iter().enumerate().skip(1) {
+            res.push(v + res[i - 1]);
+        }
+        
+        return res;
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let nums: Vec<i32> = vec![1,2,3,4];
+    let s = Solution::running_sum(nums);
+    
+    println!("array is {:?}", s);
 }
